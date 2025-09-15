cf727b40ae1de3c9c61bd3f659feb8b4c8d79ef6
winterview8990 <winterview8990@gmail.com>
2025-09-15 20:33:51 +0900

    first commit

--- src/app/ssg/page.jsx
+++ src/app/ssg/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const SSGPage = () => {
-  return <div>SSGPage</div>;
+const SSGPage = async () => {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await data.json();
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default SSGPage;
